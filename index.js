78f342c3f0abddba758951b4e2d8dd4ba653e3a8
jhelbrink <john.helbrink@gmail.com>
2017-05-15 19:32:08 +0200

    fix

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express')
+var bodyparser = require('body-parser')
 var app = express()
  
+app.use(bodyparser.json())
+
 app.get('/search', function (req, res) {
-  res.send('Search stuff here')
+  res.render('writerhome')
 })
 
 app.post('/search', function (req, res) {
-  res.send('Create search stuff here')
+
+  res.send(console.log('Title ' + req.body))
 })
+app.set('view engine', 'ejs')	
  
 app.listen(3000)
