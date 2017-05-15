97709c39207aae9dbc1bd25343d21b2b4b60cff7
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-05-15 19:23:46 +0200

    Added database files

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-var express = require('express')
-var app = express()
- 
+var express = require('express');
+var database = require('./database')
+var app = express();
+
+database.read_everything_from_table(function (err, result) {
+})
+
 app.get('/search', function (req, res) {
   res.send('Search stuff here')
-})
+});
 
 app.post('/search', function (req, res) {
   res.send('Create search stuff here')
-})
+});
  
-app.listen(3000)
+app.listen(3000);
