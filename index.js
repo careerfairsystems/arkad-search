d3faa094c460ba05a070434eccfeb22cb427c104
jhelbrink <john.helbrink@gmail.com>
2017-05-22 18:09:36 +0200

    added functionality

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 var express = require('express')
-
+var bodyParser = require('body-parser')
 var app = express()
- 
-app.get('/search', function (req, res) {
-  res.render('writerhome')
+
+var fs = require('fs')
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+app.set('view engine', 'ejs')
+
+
+app.get('/search', function(req, res) {
+    res.render('writerhome')
 })
 
-app.post('/search', function (req, res) {
+app.post('/search', function(req, res) {
 
-  res.send('hej')
+    if (req.body.title != "") {
+        var createEvent = {
+            "Title": req.body.title,
+            "Info": req.body.info,
+            "Time": req.body.time
+        }
+        saveJson(createEvent)
+        res.render('writerhome')
+    } else {
+        res.render('writerhome')
+    }
+
 })
-app.set('view engine', 'ejs')	
- 
+
+//saving to textfile. (json) 
+function saveJson(createEvent) {
+    fs.writeFile("test.txt", JSON.stringify(createEvent), function(err) {
+        if (err) {
+            return console.log(err);
+        }
+        return console.log("post was created")
+
+    })
+
+}
+
+
+
+
 app.listen(3000)
