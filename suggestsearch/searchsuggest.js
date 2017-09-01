6b5d722a51c6915833872ca84fb1a61c8a07623c
jhelbrink <john.helbrink@gmail.com>
2017-09-01 12:53:20 +0200

    suggestedsearch

---
+++ suggestsearch/searchsuggest.js
@@ -0,0 +1,98 @@
+var search = null;
+(function($, _M) {
+  'use strict';
+
+  var $searchbar = $('#search');
+
+
+
+  var dex = [{
+    name: 'Lunch: Måndag',
+    nat: 'Maträtt1',
+    index: 1,
+    cat: 'Lunch',
+    language: ['Plats 1', 'Plats 2']//language is placeholder for Tetra Pak
+  }, {
+    name: 'Lunch: Tisdag',
+    nat: 'Maträtt 2',
+    index: 2,
+    cat: 'Lunch',
+    language: ['Plats 2']//language is placeholder for Tetra Pak
+  }, {
+    name: 'Lunch: Onsdag',
+    nat: 'Maträtt 3',
+    index: 3,
+    cat: 'Lunch',
+    language: ['Plats 2', 'Plats 3']//language is placeholder for Tetra Pak
+  }, {
+    name: 'Lunch: Torsdag',
+    nat: 'Maträtt 4',
+    index: 4,
+    cat: 'Lunch',
+    language: ['Plats 4']//language is placeholder for Tetra Pak
+  }, {
+    name: 'Gasque',
+    nat: 'Mörk kostym',
+    index: 5,
+    cat: 'Middag',
+    language: ['Plats 6',] //language is placeholder for Tetra Pak
+
+
+  }];
+
+  var options = {
+    shouldSort: true,
+    threshold: 0.5,
+    minMatchCharLength: 0,
+    keys: [{
+      name: 'name',
+      weight: 0.75
+    }, {
+      name: 'nat',
+      weight: 0.5
+    }, {
+      name: 'cat',
+      weight: 0.5
+    }, {
+      name: 'language',
+      weight: 0.25
+    }]
+  };
+  var fuse = new Fuse(dex, options);
+
+  var json = (function () { //hämta json från server (måste sätta upp servern.)
+    var json = null;
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': 'example_data.json',
+        'dataType': "json",
+        'success': function (data) {
+            json = data;
+        }
+    });
+    return json;
+})(); 
+
+  $searchbar.keyup(function() {
+    var search = fuse.search($searchbar.val());
+    var $res = $('#results');
+    var check = 0;
+    $res.empty();
+    if (search[0] === undefined && ($searchbar.val()) ) {
+      $res.append('<h4 class="rip">No results</h4>');
+    }
+    $res.append('<h4>Closest result: ' + search[0].name + '</h4>');
+    $res.append('<h5>All results:</h5>');
+
+    search.forEach(function(el){
+      if(check < 5) {
+        check ++;
+      $res.append(
+        el.nat + ': ' + el.name + ' (' + el.language + ') <br>'
+      );
+    }
+    });
+  });
+
+})(jQuery, Materialize)
