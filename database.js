211e10e325904bb76b3f411f6b8548abbfa2ecc4
Oscar Rydh <oscar.rydh.93@gmail.com>
2017-10-10 09:45:04 +0200

    Refactored database connection, now you can call this.setup_connection instead use the client callback

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ var pg = require('pg')
 
 var database = {
 
-    insert_search_into_database: function (name, time, date, info, callback) {
+    setup_connection: function(callback) {
         let connectionString = "";
         if (process.env.NODE_ENV == "production") {
             connectionString = process.env.DATABASE_URL;
@@ -11,105 +11,80 @@ var database = {
             const pw = process.env.PSQLPW;
             connectionString = 'postgres://'+user+':'+pw+'@localhost:5432/arkad-search';
         }
-        var client = new pg.Client(connectionString);
-        client.connect(function (err) {
-            if (err) return callback(err, null);
+        return callback(new pg.Client(connectionString));
+    },
 
-            // execute a query on our database
-            console.log('RUNNING query');
-            client.query('INSERT INTO data(name, time, date, info) VALUES($1, $2, $3, $4)',
-                [name, time, date, info]
-                , function (err, result) {
+    insert_search_into_database: function (name, time, date, info, callback) {
+        this.setup_connection(client => {
+        client.connect(function (err) {
                 if (err) return callback(err, null);
-                return callback(null, result)
+
+                // execute a query on our database
+                client.query('INSERT INTO data(name, time, date, info) VALUES($1, $2, $3, $4)',
+                    [name, time, date, info]
+                    , function (err, result) {
+                    if (err) return callback(err, null);
+                    return callback(null, result)
+                })
             })
         })
     },
     get_user: function(username, callback) {
-        let connectionString = "";
-        if (process.env.NODE_ENV == "production") {
-            connectionString = process.env.DATABASE_URL;
-        } else {
-            const user = process.env.PSQLUSER;
-            const pw = process.env.PSQLPW;
-            connectionString = 'postgres://'+user+':'+pw+'@localhost:5432/arkad-search';
-        }        
-        var client = new pg.Client(connectionString);
+        this.setup_connection(client => {
         client.connect(function (err) {
-            if (err) return callback(err, null);
-
-            // execute a query on our database
-            console.log('RUNNING query');
-            client.query('SELECT * FROM users where username = $1 ',
-                [username]
-                , function (err, result) {
                 if (err) return callback(err, null);
-                return callback(null, result)
+
+                // execute a query on our database
+                client.query('SELECT * FROM users where username = $1 ',
+                    [username]
+                    , function (err, result) {
+                    if (err) return callback(err, null);
+                    return callback(null, result)
+                })
             })
         })
     },
 
     add_user: function(username, password, callback) {
-        let connectionString = "";
-        if (process.env.NODE_ENV == "production") {
-            connectionString = process.env.DATABASE_URL;
-        } else {
-            const user = process.env.PSQLUSER;
-            const pw = process.env.PSQLPW;
-            connectionString = 'postgres://'+user+':'+pw+'@localhost:5432/arkad-search';
-        }
-        const client = new pg.Client(connectionString);
-        client.connect(function (err) {
-            if (err) return callback(err, null);
-
-            // execute a query on our database
-            client.query('INSERT INTO users(username, password) VALUES($1, $2);',
-                [username, password]
-                , function (err, result) {
+        this.setup_connection(client => {
+            client.connect(function (err) {
                 if (err) return callback(err, null);
-                return callback(null, result)
+
+                // execute a query on our database
+                client.query('INSERT INTO users(username, password) VALUES($1, $2);',
+                    [username, password]
+                    , function (err, result) {
+                    if (err) return callback(err, null);
+                    return callback(null, result)
+                });
             });
-        });
+        })
     },
 
     read_everything_from_table: function (callback) {
-        let connectionString = "";
-        if (process.env.NODE_ENV == "production") {
-            connectionString = process.env.DATABASE_URL;
-        } else {
-            const user = process.env.PSQLUSER;
-            const pw = process.env.PSQLPW;
-            connectionString = 'postgres://'+user+':'+pw+'@localhost:5432/arkad-search';
-        }
-        var client = new pg.Client(connectionString);
-        client.connect(function (err) {
-            if (err) return callback(err, null);
-
-            // execute a query on our database
-            client.query('SELECT * FROM data;', function (err, result) {
+        this.setup_connection(client => {
+            client.connect(function (err) {
                 if (err) return callback(err, null);
-                return callback(null, result.rows)
+
+                // execute a query on our database
+                client.query('SELECT * FROM data;', function (err, result) {
+                    if (err) return callback(err, null);
+                    return callback(null, result.rows)
+                })
             })
         })
     },
 
     read_names_from_table: function (callback) {
-        let connectionString = "";
-        if (process.env.NODE_ENV == "production") {
-            connectionString = process.env.DATABASE_URL;
-        } else {
-            const user = process.env.PSQLUSER;
-            const pw = process.env.PSQLPW;
-            connectionString = 'postgres://'+user+':'+pw+'@localhost:5432/arkad-search';
-        }
-        var client = new pg.Client(connectionString);
-        client.connect(function (err) {
-            if (err) return callback(err, null);
-
-            // execute a query on our database
-            client.query('SELECT name FROM data;', function (err, result) {
+        this.setup_connection(client => {
+            client.connect(function (err) {
                 if (err) return callback(err, null);
-                return callback(null, result.rows)
+
+                // execute a query on our database
+                client.query('SELECT name FROM data;', function (err, result) {
+                    if (err) return callback(err, null);
+                    return callback(null, result.rows)
+                })
             })
         })
     }
