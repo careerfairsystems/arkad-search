e26268fcea2b566160cfc22c6fc122ceb4293da9
Peter Skopal <peter.skopal@hotmail.com>
2017-10-17 21:03:51 +0200

    fix(merge-commits): Error handling merged together.

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ var Fuse = require('fuse.js');
 var fuseOptions = require('./fuse-option.json');
 var session = require('express-session');
 var bcrypt = require('bcrypt');
+var Raven = require('raven');
+
+// Setting up Error Handling
+Raven.config(process.env.SENTRY_DSN).install();
+app.use(Raven.requestHandler());
+app.use(Raven.errorHandler());
+// Optional fallthrough error handler
+function throwError(err, res) {
+    Raven.captureException(err);
+    return res.render('error');
+}
 
 const saltRounds = 10;
 
@@ -18,7 +29,6 @@ app.use( session({
     resave            : false,
     saveUninitialized : true
 }));
-
 
 // Login middleware for /contet
 var login = (req, res, next) => {
@@ -41,45 +51,45 @@ app.get('/login', (req, res) => {
     } else {
         res.redirect('/content');
     }
-})
+});
 
 app.get('/', (req, res) => {
     res.redirect('/login');
-})
+});
 
 app.post('/logout', (req,res) => {
     req.session.destroy((err) => {
-        if (err) return res.render('error')
+        if (err) return throwError(err, res);
         res.redirect('/login');
-    })
-})
+    });
+});
 
-app.get('/create_account', (reg,res) => {
-    res.render('create_admin');
-})
+app.get('/create_account', login, (reg,res) => {
+    res.render('create_user');
+});
 
 app.post('/create_account', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     // Hashing and salting password
     bcrypt.hash(password, saltRounds, (err, hash) => {
-        if (err) return res.render('error')
+        if (err) return throwError(err, res);
         db.add_user(username, hash, (err, result) => {
-            if (err) return res.render('error');
+            if (err) return throwError(err, res);
             res.redirect('/login');
-        })
+        });
     });
-})
+});
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     db.get_user(username, (err, result) => {
-        if (err) return res.render('error');
+        if (err) return throwError(err, res);
         if (result.rows.length != 0) {
             const hash = result.rows[0].password;
             bcrypt.compare(password, hash, (err, match) => {
-                if (err) return res.render('error');
+                if (err) return throwError(err, res);
                 if (match) {
                     req.session.regenerate((err) => {
                         req.session.isLoggedIn = true;
@@ -89,16 +99,16 @@ app.post('/login', (req, res) => {
                 } else {
                     res.redirect('/login');
                 }
-            })
+            });
         } else {
             res.redirect('/login');
         }
-    })
-})
+    });
+});
 
 app.get('/search', login, function(req, res) {
-    res.render('writerhome')
-})
+    res.render('post_add')
+});
 
 app.post('/search', login, function(req, res) {
 
@@ -110,31 +120,33 @@ app.post('/search', login, function(req, res) {
             "Info": req.body.info
         }
         db.insert_search_into_database(createEvent.Name,createEvent.Time,createEvent.Date,createEvent.Info, function(err, result){
-            if(err) return res.render('error');
-            return res.render('writerhome')
-        })
+            if(err) return throwError(err, res);
+            return res.render('post_add')
+        });
     } else {
-        res.render('writerhome')
+        res.render('post_add')
     }
-})
+});
 
 app.get('/content', login,function(req,res){
     db.read_everything_from_table(function (err, result) {
+        if (err) return throwError(err, res);
+
         var dbcontent = result;
-        res.render('database_content',{
+        res.render('content',{
             dbcontent: dbcontent
         });
-    })
-})
+    });
+});
 
 app.get('/edit/:id', login, function(req, res) {
     db.read_entry(req.params.id, function(err, result) {
-        if (err) return res.render('error');
-        return res.render('edit_post', {
+        if (err) return throwError(err, res);
+        return res.render('post_edit', {
             entry: result.rows[0]
         });
     });
-})
+});
 
 app.post('/edit/:id', login, function(req, res) {
     db.update_entry({
@@ -144,22 +156,22 @@ app.post('/edit/:id', login, function(req, res) {
         date: req.body.date,
         info: req.body.info
      }, function(err, result) {
-        if (err) return res.render('error');
+        if (err) return throwError(err, res);
         return res.redirect('/content');
     });
-})
+});
 
 app.post('/delete/:id', login, function(req, res) {
     db.delete_search(req.params.id, function(err, result) {
-        if (err) return res.render('error');
+        if (err) return throwError(err, res);
         return res.redirect('/content');
     });
-})
+});
 
 
 app.get('/arkad-search/:searchTerm', function(req,res){
     db.read_everything_from_table(function (err, result) {
-        if(err) return res.render('error');
+        if (err) return throwError(err, res);
         var fuse = new Fuse(result, fuseOptions);
         res.json(fuse.search(req.params.searchTerm));
     })
@@ -167,16 +179,17 @@ app.get('/arkad-search/:searchTerm', function(req,res){
 
 app.get('/users', login,function(req,res){
     db.read_users_from_table(function (err, result) {
+        if (err) return throwError(err, res);
         var dbusers = result;
         dbusers.sort(function(a, b) {
             return a.username > b.username;
         })
-        res.render('database_users',{
+        res.render('users',{
             dbusers: dbusers,
             isAdmin: req.session.isAdmin
         });
-    })
-})
+    });
+});
 
 app.post('/admin_permission', login, function(req, res){
   db.update_admin_permission(
@@ -184,24 +197,24 @@ app.post('/admin_permission', login, function(req, res){
       username: req.query.username,
       admin: req.query.admin
     }, function(err, result) {
-        if (err) return res.render('error');
+        if (err) return throwError(err, res);
         return res.redirect('/users');
     });
-})
+});
 
 app.post('/delete_user', login, function(req, res){
   db.delete_user({
       username: req.body.username
   }, function(err, result){
-      if (err) return res.render('error');
+      if (err) return throwError(err, res);
       return res.json({redirect: '/users'});
   });
-})
+});
 
 // This needs to be last!
 app.use((req, res) => {
     res.render('404');
-})
+});
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
